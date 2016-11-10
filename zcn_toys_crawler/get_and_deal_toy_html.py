9cdbd69129bf4470d3792572adb3a78f21441acd
Foristkirito <xiaxin0202@foxmail.com>
2016-11-10 21:58:27 +0800

    细节爬去完成

--- zcn_toys_crawler/get_and_deal_toy_html.py
+++ zcn_toys_crawler/get_and_deal_toy_html.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -8,76 +8,157 @@ from bs4 import *
 import bs4
 import csv
 import codecs
+import time
 
-fout = file("toylist.txt", 'r')
+fout = file("list.test", 'r')
 csvfile = file('toys.csv', 'wb')
+log_file = file('n_normal.log', 'w')
+pass_link = file('pass_link.txt', 'w')
 csvfile.write(codecs.BOM_UTF8)
 writer = csv.writer(csvfile)
-
-get_list = [
-	u'productTitle',
-	u'brand',
-	u'avgRating',
-	u'acrCustomerReviewText',
-	u'priceblock_ourprice',
-	u'imgTagWrapperId',
-	u'productDescription',
-	
-	u'ddmMerchantMessage'
-]
-
 links_set = fout.readlines()
+pass_count = 0
 
-# for i in range(1,300):
-# 	print url_preffix1.format(i)
-# 	opener = urllib2.build_opener()
-# 	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-# 	html = opener.open(url_preffix1.format(i))
-# 	# html = urllib2.urlopen(url_preffix1.format(i))
-# 	txt = html.read()
-# 	if i % 20 == 0:
-# 		print (i/20)
-# 	soup = BeautifulSoup(txt, 'html.parser')
-# 	links = soup.findAll("a", class_='a-link-normal s-access-detail-page  a-text-normal')
-# 	for j in links:
-# 		if j['href'] not in links_set:
-# 			links_set.add(j['href'])
+def normalTag(soup, line, tag):
+	divs = soup.findAll(id = tag)
+	if len(divs) == 0:
+		line.append("")
+	else:
+		name = divs[0].text.strip()
+		try:
+			line.append(name)
+		except:
+			line.append("")
+	return line
+
+def productName(soup, line):
+	return normalTag(soup, line, u'productTitle')
+
+def productBrand(soup, line):
+	return normalTag(soup, line, u'brand')
+
+def produceRate(soup, line):
+	return normalTag(soup, line, u'avgRating')
+
+def reviewNumber(soup, line):
+	return normalTag(soup, line, u'acrCustomerReviewText')
+
+def recoPrice(soup, line):
+	price_re = soup.find(id = "price")
+	price_t = price_re.findAll('table')
+	for row in price_t:
+		cols = row.find_all('td')
+		if cols[0].get_text() == u'厂商建议零售价:':
+			line.append(u'厂商建议零售价:' + cols[1].get_text().strip())
+			return line
+		else:
+			line.append("")
+			return line
+
+def priceSale(soup ,line):
+	price = ""
+	divs = soup.findAll(id = u'priceblock_ourprice')
+	if len(divs) == 0:
+		divs = soup.findAll(id = u'priceblock_saleprice')
+		price = ""
+	if len(divs) > 0:
+		name = divs[0].text.strip()
+		try:
+			price = name
+		except:
+			pass
+	line.append(price)
+	return line
+
+
+def productDescription(soup, line):
+	totalDes = soup.find(id = "productDescription_feature_div")
+	[x.extract() for x in totalDes.findAll('script')]
+	[x.extract() for x in totalDes.findAll('style')]
+	h2 = totalDes.findAll('div', class_='disclaim')
+	#line.append(h2[0].get_text()).strip()
+	if len(h2) > 0:
+		line.append(h2[0].get_text())
+	else:
+		line.append("")
+	subTitles = totalDes.findAll('h3')
+	for title in subTitles:
+		line.append(title.get_text().strip() + title.next_sibling.next_sibling.get_text().strip())
+	return line
+
+def offerDescription(soup, line):
+	dsp = ""
+	divs = soup.find(id = u'aplus')
+	if divs:
+		[x.extract() for x in divs.findAll('script')]
+		[x.extract() for x in divs.findAll('style')]
+		for j in divs:
+			try:
+				dsp = dsp + "\n" + j.get_text().strip()
+			except:
+				pass
+	line.append(dsp)
+	return line
+
+def baseInfo(soup, line):
+	descript = soup.find(id = "detail_bullets_id")
+	[x.extract() for x in descript.findAll('script')]
+	[x.extract() for x in descript.findAll('style')]
+	lists = descript.findAll('li')
+	for li in lists:
+		try:
+			line.append(li.get_text().strip())
+			if str(li.get_text()).find("产地") > -1 :
+				break
+		except:
+			pass
+	return line
+
+def productRak(soup, line):
+	rank = soup.find(id = "SalesRank")
+	[x.extract() for x in rank.findAll('a')]
+	[x.extract() for x in rank.findAll('script')]
+	[x.extract() for x in rank.findAll('style')]
+	[x.extract() for x in rank.findAll('ul')]
+	line.append(rank.get_text().strip())
+	return line
+
+
 
 for i in links_set:
+	print "------------------------------------"
 	print i
-	opener = urllib2.build_opener()
-	opener.addheaders = [('User-agent', 'Chrome/51.0.2704.63')]
-	html = opener.open(i)
-	html = urllib2.urlopen(i)
-	txt = html.read()
+	txt = ""
+	try:
+		opener = urllib2.build_opener()
+		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		html = opener.open(i)
+		txt = html.read()
+	except:
+		log_file.write(txt)
+		pass_link.write(i)
+		pass_count = pass_count + 1
+		continue
 	soup = BeautifulSoup(txt, 'html.parser')
 	line = []
 	#***************#
-	for i in get_list:
-		divs = soup.findAll(id = i)
-		if len(divs) == 0:
-			line.append("")
-			continue
-		name = divs[0].text.strip()
-		print name
-		if name:
-			line.append(name)
-		else:
-			line.append("")
-	#***************#
-	classify = soup.findAll("a",  class_="a-link-normal a-color-tertiary")
-	if len(classify) == 3:
-		line.append(classify[2].text.strip())
-	else:
-		line.append("")
-	#***************#
-	# table = soup.findAll("td", class_="bucket")[0].div.findAll('li')
-	# for i in table:
-	# 	pass
+	productName(soup, line)
+	productBrand(soup, line)
+	produceRate(soup, line)
+	reviewNumber(soup, line)
+	recoPrice(soup, line)
+	priceSale(soup, line)
+	productDescription(soup, line)
+	offerDescription(soup, line)
+	baseInfo(soup, line)
+	productRak(soup, line)
+	line.append(i)
 	writer.writerow(line)
+	time.sleep(2)
+
+print "pass number : " + str(pass_count)
 
 fout.close()
 csvfile.close()
-
-
-
+log_file.close()
+pass_link.close()
--- zcn_toys_crawler/test.py
+++ zcn_toys_crawler/test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -10,44 +10,83 @@ import csv
 import codecs
 import re
 
-fout = file("list.test", 'r')
 log_file = file("test.log", "w")
 
 get_list = [
 	u'aplus'
 ]
 
-links_set = fout.readlines()
 
 
-for i in links_set:
-	print i
-	opener = urllib2.build_opener()
-	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-	opener.addheaders.append(('Cookie', 'cookiename=cookievalue'))
-	html = opener.open(i)
-	#html = urllib2.urlopen(i)
-	txt = html.read()
-	log_file.write(txt)
-	print txt
-	soup = BeautifulSoup(txt, 'html.parser')
-	line = []
-	#***************#
-	for i in get_list:
-		print i
-		divs = soup.find(id = u'brand')
-		print divs
-		cleanr = re.compile('<.*?>')
-		#cleantext = re.sub(cleanr, '', divs)
-		#print cleantext
-		
-	#***************#
-	#***************#
-	# table = soup.findAll("td", class_="bucket")[0].div.findAll('li')
-	# for i in table:
-	# 	pass
+#print i
+#opener = urllib2.build_opener()
+#opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+#opener.addheaders.append(('Cookie', 'cookiename=cookievalue'))
+#html = opener.open(i)
+#html = urllib2.urlopen(i)
+#txt = html.read()
 
-fout.close()
+#获得建议零售价
+txt = codecs.open("test.html", 'r').read()
+log_file.write(txt)
+soup = BeautifulSoup(txt, 'html.parser')
+line = []
+price_re = soup.find(id = "price")
+price_t = price_re.findAll('table')
+flag = False
+#for row in price_t:
+#	cols = row.find_all('td')
+#	if cols[0].get_text() == u'厂商建议零售价:':
+#		print cols[1].get_text()
+
+
+# 获得描述信息
+#for i in get_list:
+#	divs = soup.find(id = u'aplus')
+#	[x.extract() for x in divs.findAll('script')]
+#	[x.extract() for x in divs.findAll('style')]
+#	for j in divs:
+#		try:
+#			print j.get_text()
+#		except:
+#			pass
+
+#逐条获得基本信息
+descript = soup.find(id = "detail_bullets_id")
+#print descript
+'''
+[x.extract() for x in descript.findAll('script')]
+[x.extract() for x in descript.findAll('style')]
+lists = descript.findAll('li')
+for li in lists:
+	try:
+		print "-------"
+		print li.get_text()
+		if str(li.get_text()).find("产地") > -1:
+			break;
+	except:
+		pass
+'''
+
+# 获得排名
+rank = soup.find(id = "SalesRank")
+[x.extract() for x in rank.findAll('a')]
+[x.extract() for x in rank.findAll('script')]
+[x.extract() for x in rank.findAll('style')]
+[x.extract() for x in rank.findAll('ul')]
+print rank.get_text().strip()
+
+# 逐条获得基本信息
+#totalDes = soup.find(id = "productDescription_feature_div")
+#[x.extract() for x in totalDes.findAll('script')]
+#[x.extract() for x in totalDes.findAll('style')]
+#h2 = totalDes.findAll('div', class_='disclaim')
+#print h2[0].get_text()
+#subTitles = totalDes.findAll('h3')
+#for title in subTitles:
+#	print title.get_text()
+#	print title.next_sibling.next_sibling.get_text()
+
+
+
 log_file.close()
-
-
